b6fb98b8b07e0b4945c46fce94c58e30c3a2dc37
Bhargavvallapuram <147648796+Bhargavvallapuram@users.noreply.github.com>
2025-01-17 21:29:25 +0530

    Update Header.jsx

--- src/Header.jsx
+++ src/Header.jsx
@@ -17,12 +17,12 @@ const Header = ({ onNavClick }) => {
       </button>
       <nav className={`navbar ${menuActive ? 'active' : ''}`}>
         <ul className="nav-links">
-          <li><Link to="/" onClick={() => setMenuActive(false)}>Home</Link></li>
-          <li><Link to="/projects" onClick={() => setMenuActive(false)}>Projects</Link></li>
-          <li><Link to="/certificates" onClick={() => setMenuActive(false)}>Certificates</Link></li>
-          <li><Link to="/about" onClick={() => setMenuActive(false)}>About Me</Link></li>
-          <li><Link to="/services" onClick={() => setMenuActive(false)}>Services</Link></li>
-          <li><Link to="/contact" onClick={() => setMenuActive(false)}>Contact</Link></li>
+          <li><Link to="/#" onClick={() => setMenuActive(false)}>Home</Link></li>
+          <li><Link to="/#projects" onClick={() => setMenuActive(false)}>Projects</Link></li>
+          <li><Link to="/#certificates" onClick={() => setMenuActive(false)}>Certificates</Link></li>
+          <li><Link to="/#about" onClick={() => setMenuActive(false)}>About Me</Link></li>
+          <li><Link to="/#services" onClick={() => setMenuActive(false)}>Services</Link></li>
+          <li><Link to="/#contact" onClick={() => setMenuActive(false)}>Contact</Link></li>
         </ul>
       </nav>
     </header>
