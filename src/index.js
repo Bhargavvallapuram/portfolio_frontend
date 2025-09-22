24de8a4085648695567f8e5aa4e84bd209eb559f
Bhargavvallapuram <147648796+Bhargavvallapuram@users.noreply.github.com>
2025-09-22 22:26:43 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles.css';
 import Header from './Header';
-import Footer from './footer';
+import Footer from './Footer';
 import Home from './Home';
 import Project from './Project';
 import Certificate from './Certificate';
 import About from './About';
 import ContactMe from './ContactMe';
 import Services from './Services';
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Changed HashRouter to BrowserRouter
 
 const App = () => {
   return (
@@ -23,7 +23,14 @@ const App = () => {
           <Route path="/about" element={<About />} />
           <Route path="/services" element={<Services />} />
           <Route path="/contact" element={<ContactMe />} />
-          <Route path="*" element={<h2 style={{ textAlign: 'center', marginTop: '50px' }}>Page Not Found</h2>} />
+          <Route
+            path="*"
+            element={
+              <h2 style={{ textAlign: 'center', marginTop: '50px' }}>
+                Page Not Found
+              </h2>
+            }
+          />
         </Routes>
       </main>
       <Footer />
